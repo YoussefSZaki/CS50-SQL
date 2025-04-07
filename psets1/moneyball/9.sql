b9b8d4ed439ec32084361678c08be356b1029625
YoussefSZaki <145165633+YoussefSZaki@users.noreply.github.com>
2025-04-07 16:58:10 +0200

    Create 9.sql

---
+++ psets1/moneyball/9.sql
@@ -0,0 +1,7 @@
+SELECT "name", ROUND(AVG("salary"), 2) AS "average salary" FROM "teams"
+JOIN "salaries" ON "salaries"."team_id" = "teams"."id"
+WHERE "salaries"."year" = 2001
+GROUP BY "team_id"
+ORDER BY "average salary" ASC
+LIMIT 5
+;
