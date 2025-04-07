816ab25659d661bc22e50c7c20eb278fc37f20ba
YoussefSZaki <145165633+YoussefSZaki@users.noreply.github.com>
2025-04-07 16:47:15 +0200

    Create 13.sql

---
+++ psets1/dese/13.sql
@@ -0,0 +1,4 @@
+SELECT "name", "dropped", "per_pupil_expenditure" FROM "schools"
+JOIN "graduation_rates" ON "graduation_rates"."school_id" = "schools"."id"
+JOIN "expenditures" ON "expenditures"."district_id" = "schools"."district_id"
+ORDER BY "per_pupil_expenditure" ASC, "dropped" DESC;
