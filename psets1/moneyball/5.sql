9d9e79591b597fc6a2c8c4e8cc782470992e51af
YoussefSZaki <145165633+YoussefSZaki@users.noreply.github.com>
2025-04-07 16:57:06 +0200

    Create 5.sql

---
+++ psets1/moneyball/5.sql
@@ -0,0 +1,6 @@
+SELECT DISTINCT "name" FROM "teams"
+JOIN "performances" ON "performances"."team_id" = "teams"."id"
+JOIN "players" ON "players"."id" = "performances"."player_id"
+WHERE "first_name" = 'Satchel' AND "last_name" = 'Paige'
+GROUP BY "name"
+;
