5d3051744abc7bb057f8edc79f650385cd501341
YoussefSZaki <145165633+YoussefSZaki@users.noreply.github.com>
2025-05-27 12:51:50 +0300

    Delete Files

--- psets0/cyberchase/13.sql
+++
@@ -1,1 +0,0 @@
-SELECT "title" FROM "episodes" WHERE ("air_date" LIKE '2019%' OR "air_date" LIKE '2020%');
--- psets0/cyberchase/6.sql
+++
@@ -1,1 +0,0 @@
-SELECT "title" FROM "episodes" WHERE ("air_date" LIKE '2007%' AND "season" = '6');
