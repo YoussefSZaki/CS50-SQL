f418e6464a964db671ed31c091823c840a0a7e7f
YoussefSZaki <145165633+YoussefSZaki@users.noreply.github.com>
2025-05-27 13:44:33 +0300

    Delete Files

--- psets1/moneyball/11.sql
+++
@@ -1,6 +0,0 @@
-SELECT "first_name", "last_name", "salary"/"H" AS "dollars per hit" FROM "performances"
-JOIN "salaries" ON "salaries"."player_id" = "performances"."player_id" AND "salaries"."year" = "performances"."year"
-JOIN "players" ON "players"."id" = "performances"."player_id"
-WHERE "H" > 0 AND "performances"."year" = '2001'
-ORDER BY "dollars per hit" ASC, "first_name" ASC, "last_name" ASC
-LIMIT 10;
